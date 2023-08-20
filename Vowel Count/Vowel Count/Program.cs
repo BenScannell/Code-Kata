ec881c5600e2eb3dcc4e6511001060eb377a397c
Ben <benscannell@yahoo.com>
2023-08-20 14:03:07 +0100

    Add "Vowel Count"

---
+++ Vowel Count/Vowel Count/Program.cs
@@ -0,0 +1,19 @@
+﻿//Kata: Vowel Count
+//Task: Create a function to count how many vowels are in a given input
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        return Regex.Replace(str, "[bcdfghjklmnpqrstvwxyz ]", "").Length;
+    }
+    public static void Main()
+    {
+        int output = GetVowelCount("pear tree");
+        Console.WriteLine(output);
+    }
+}
