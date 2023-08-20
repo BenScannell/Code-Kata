6f0520dfdd88f7719e67db03ab0c98b3cdd3a9b5
Ben <57094806+BenScannell@users.noreply.github.com>
2023-08-20 13:09:31 +0100

    Add all previously completed Kata

---
+++ Disemvowel Trolls/Disemvowel Trolls/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public static class Main
+{
+    //Kata: Disemvowel Trolls
+    //Task: Create a function to remove any vowels from a string passed to it
+    public static string Disemvowel(string str)
+    {
+        string[] vovels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+        for (int i = 0; i < vovels.Length; i++) {
+            str = str.Replace(vovels[i], "");
+        }
+        return str;
+    }
+}
