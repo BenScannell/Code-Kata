fd929db88c85af56b966dbc583360b594bcca4d7
Ben <benscannell@yahoo.com>
2023-09-04 13:13:11 +0100

    Create Digital Roots.cs

---
+++ Digital Roots.cs
@@ -0,0 +1,23 @@
+﻿//Kata: Sum of Digits / Digital Root
+//Task: Create a function to find the digital root of an entered number (Add the sum of the digits until only one digit remains).
+public class Number
+{
+    public static long DigitalRoot(long n)
+    {
+        while (n.ToString().Length > 1)
+        {
+            string strvalue = n.ToString();
+            int[] digitarray = new int[strvalue.Length];
+            for (int i = 0; i < strvalue.Length; i++)
+            {
+                digitarray[i] = int.Parse(strvalue[i].ToString());
+            }
+            n = digitarray.Sum();
+        }
+        return n;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(DigitalRoot(167346));
+    }
+}
